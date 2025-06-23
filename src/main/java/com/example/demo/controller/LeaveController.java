631b2106d30a82ecd4989d4406d1bd08d11c6f0f
yunfeng <zhangyunfeng@payworth.com>
2025-06-23 13:48:50 +0800

    qry

--- src/main/java/com/example/demo/controller/LeaveController.java
+++ src/main/java/com/example/demo/controller/LeaveController.java
@@ -3,21 +3,32 @@ package com.example.demo.controller;
 import com.example.demo.util.Result;
 import com.example.demo.vo.TaskVO;
 import lombok.extern.slf4j.Slf4j;
-
-import org.apache.commons.collections.CollectionUtils;
 import org.flowable.bpmn.model.BpmnModel;
-import org.flowable.engine.*;
+import org.flowable.engine.ProcessEngine;
+import org.flowable.engine.ProcessEngineConfiguration;
+import org.flowable.engine.RepositoryService;
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.TaskService;
 import org.flowable.engine.runtime.Execution;
 import org.flowable.engine.runtime.ProcessInstance;
 import org.flowable.image.ProcessDiagramGenerator;
 import org.flowable.task.api.Task;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RestController
